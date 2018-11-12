810740cb8898fb49efd9be7f8747b3a4d0e11b72
U-matt-PC\matt <mattpkqwe@gmail.com>
2018-11-12 00:38:01 -0500

    mvp

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # morning-step
 creating accountability for myself with each day starting with a step that i want to take
 
-todo:
-two states based on time: 2pm-6am, able to set a morning task
-6am-2pm, able to complete a morning task
-completing a morning task means incrementing a counter. wohoo.
+https://step.mchung.ca
+
+## todo:
+two states based on time : 6am-noon, able to complete a morning task
+if completed / not time, can set.
+
+Change Save button to Complete button.
+when complete, add timestamp to list
+do math and display streak
 
 create server
 use redis key-value storage
 send email each morning with reminder
 
-this is a proof of concep, so i'm using https://keyvalue.xyz/ instead of rolling my own server.
+this is a proof of concept, so i'm using https://keyvalue.xyz/ instead of rolling my own server.
--- script.js
+++ script.js
@@ -1,23 +1,65 @@
+const MIN_COMPLETION_HOUR = 6;
+const MAX_COMPLETION_HOUR = 12;
+
 class MorningApp extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {step: props.step, saved: false};
+    const date = new Date();
+    this.state = {
+      step: props.step,
+      saved: false,
+      date: date,
+      canComplete: false,
+      completed: false,
+      // a list of Dates
+      completions: []
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-
+    this.validHours = this.validHours.bind(this);
+    this.checkCanComplete = this.checkCanComplete.bind(this);
   }
+
+  checkCanComplete() {
+    if (!this.validHours(this.state.date.getHours())) {
+      return false;
+    }
+    if (this.state.completions.length == 0) {
+      return true;
+    }
+    const lastCompletion = this.state.completions[this.state.completions.length - 1];
+    const hoursSince = (this.state.date - lastCompletion) / 36000;
+    if (hoursSince <= MAX_COMPLETION_HOUR - MIN_COMPLETION_HOUR) {
+      return false;
+    }
+    return true;
+  }
+
+  validHours(hours) {
+    return MIN_COMPLETION_HOUR <= hours && hours <= MAX_COMPLETION_HOUR;
+  }
+
   handleChange(event) {
     this.setState({step: event.target.value});
   }
 
   handleSubmit(event) { 
-    this.setState({saved: true});
-    fetch('https://api.keyvalue.xyz/a4408b5f/myKey', {
-      method: 'POST',
-      body: JSON.stringify({step: this.state.step}),
-    });
     event.preventDefault();
+    if (!this.state.canComplete) {
+      this.setState({saved: !this.state.canComplete, completed: this.state.canComplete});
+      fetch('https://api.keyvalue.xyz/a4408b5f/myKey', {
+        method: 'POST',
+        body: JSON.stringify({step: this.state.step}),
+      });
+    } else {
+      this.state.completions.push(new Date());
+      fetch('https://api.keyvalue.xyz/b49fdc3f/completions', {
+        method: 'POST',
+        body: JSON.stringify(this.state.completions),
+      });
+      this.setState({canComplete: false, completed: true, step: ""});
+    }
   }
 
   componentDidMount() {
@@ -28,6 +70,14 @@ class MorningApp extends React.Component {
     }).then((json) => {
       this.setState(json);
       updateTextAreaHeight();
+    });
+    fetch('https://api.keyvalue.xyz/b49fdc3f/completions', {
+      method: 'GET'
+    }).then((response) => {
+      return response.json();
+    }).then((json) => {
+      this.setState({completions: json.map(d => new Date(d))});
+      this.setState({canComplete: this.checkCanComplete()});
     });
   }
 
@@ -43,13 +93,20 @@ class MorningApp extends React.Component {
             </textarea>
             <small id="step" className="form-text text-muted">Choose a task that you'll enjoy and finish within half an hour.</small>
           </div>
-          <button type="submit" className="btn btn-primary">Save</button>
+          <button type="submit" className={this.state.canComplete? "btn btn-success" : "btn btn-primary"}>
+          { this.state.canComplete ? "Mark Completed" : "Save" }
+          </button>
         </form>
         <div></div>
 
         { this.state.saved ? 
         <div className="alert mt-2 alert-success" role="alert">
           Saved. Good luck on your morning step tomorrow.
+        </div> : null }
+
+        { this.state.completed ? 
+        <div className="alert mt-2 alert-success" role="alert">
+          Completed! Good work. That's XXX completions in a row, and {this.state.completions.length} completions so far.
         </div> : null }
 
         </div>
@@ -63,7 +120,7 @@ ReactDOM.render(React.createElement(MorningApp), domContainer);
 
 // jquery hack to fix the textbox.
 function updateTextAreaHeight() {
-  $('textarea#step').height(0).height($('textarea#step')[0].scrollHeight - 10);
+  $('textarea#step').height(0).height($(' textarea#step')[0].scrollHeight - 10);
 }
 $('.form-group').on( 'change keyup keydown paste cut', 'textarea', updateTextAreaHeight).find( 'textarea' ).change();
 $(window).resize(updateTextAreaHeight);
